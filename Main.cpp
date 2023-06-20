aba05ba8cbaae8f87720088913b24fb2646230d6
IvanShulga <59797260+IvanShulga@users.noreply.github.com>
2023-06-20 14:43:33 +0300

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "MainFunctions.h"
+
+int main(int argc, char** argv) {
+
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    program(argc, argv);
+
+    return 0;
+
+}
